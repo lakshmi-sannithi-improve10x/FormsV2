7f81af3fe4ac6f2ce8feea149d115ab901e3f605
LENOVO <lakshmi.sannithi@improve10x.com>
2023-09-01 20:11:38 +0530

    I modified my activites.

--- app/src/main/java/com/improve10x/formsv2/AddressActivity.java
+++ app/src/main/java/com/improve10x/formsv2/AddressActivity.java
@@ -1,9 +1,14 @@
 package com.improve10x.formsv2;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
@@ -15,6 +20,10 @@ public class AddressActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_address);
+        ActionBar actionBar = getSupportActionBar();
+        actionBar.setTitle("Address");
+        actionBar.setDisplayHomeAsUpEnabled(true);
+
         Intent intent = getIntent();
         Bundle bundle = intent.getExtras();
 
@@ -37,16 +46,33 @@ public class AddressActivity extends AppCompatActivity {
             String country = countryTxt.getText().toString();
             Intent birthDetailsIntent = new Intent(this, BirthDetailsActivity.class);
             birthDetailsIntent.putExtras(bundle);
-            birthDetailsIntent.putExtra("Hno",hno);
-            birthDetailsIntent.putExtra("Street",street);
-            birthDetailsIntent.putExtra("City",city);
-            birthDetailsIntent.putExtra("State",state);
-            birthDetailsIntent.putExtra("Country",country);
+            birthDetailsIntent.putExtra("Hno", hno);
+            birthDetailsIntent.putExtra("Street", street);
+            birthDetailsIntent.putExtra("City", city);
+            birthDetailsIntent.putExtra("State", state);
+            birthDetailsIntent.putExtra("Country", country);
             startActivity(birthDetailsIntent);
         });
         Button backBtn = findViewById(R.id.back_btn);
         backBtn.setOnClickListener(v -> {
             finish();
         });
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        if (item.getItemId() == android.R.id.home){
+            Toast.makeText(this, "Bye", Toast.LENGTH_LONG).show();
+            finish();
+
+        }
+        return true;
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater menuInflater = getMenuInflater();
+        menuInflater.inflate(R.menu.address_menu,menu);
+        return true;
     }
 }
